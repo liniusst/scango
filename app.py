726e7994c54e67fd01384520205c9bc3cb24964f
Linas Stonkus <liniusst@gmail.com>
2023-11-07 23:07:45 +0200

    update main function, conf ratio over 90 with good quality img

--- app.py
+++ app.py
@@ -6,76 +6,45 @@ import re
 import numpy as np
 from PIL import Image
 from typing import Dict
+import matplotlib.pyplot as plt
 
 
-def preprocess_image_for_ocr(img_path) -> Image:
-    image = cv2.imread(img_path)
-
-    model_path = os.path.join(".", "runs", "detect", "train2", "weights", "best.pt")
+def detect_on_img(img_path: os.path) -> Dict:
+    model_path = os.path.join(".", "runs", "detect", "train3", "weights", "best.pt")
     model = YOLO(model_path)
 
+    image = cv2.imread(img_path)
+
     detections = model(image)[0]
+    reader = easyocr.Reader(["en"], gpu=False)
+    license_plate_list = []
 
     for detection in detections.boxes.data.tolist():
         x1, y1, x2, y2, score, class_id = detection
         cropped_plate = image[int(y1) : int(y2), int(x1) : int(x2)]
-        gray_plate = cv2.cvtColor(cropped_plate, cv2.COLOR_BGR2GRAY)
-        gray_plate = cv2.bilateralFilter(gray_plate, 11, 17, 17)
-    return gray_plate
+        gray_plate = cv2.cvtColor(cropped_plate, cv2.COLOR_BGR2RGB)
+        _, license_plate_thresh = cv2.threshold(
+            gray_plate, 64, 255, cv2.THRESH_BINARY_INV
+        )
 
+    output = reader.readtext(license_plate_thresh, paragraph=False)
 
-def enhance_image_contrast(image) -> Image:
-    alpha = 1.5
-    beta = 0
-    enhanced_image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-    return enhanced_image
+    for out in output:
+        text_bbox, text, text_score = out
+        if text_score > 0.25:
+            license_plate_list.append(text)
 
-
-def enhance_image_brightness(image) -> Image:
-    alpha = 1.0
-    beta = 50
-    enhanced_image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-    return enhanced_image
-
-
-def enhance_image_sharpening(image) -> Image:
-    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-    enhanced_image = cv2.filter2D(image, -1, kernel)
-    return enhanced_image
-
-
-def process_image_with_ocr(image) -> Dict:
-    thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-    reader = easyocr.Reader(["en"], gpu=False)
-    ocr_result = reader.readtext(thresh, paragraph=False)
-
-    if ocr_result:
-        license_plate_text = ocr_result[0][1]
-        conf_score = ocr_result[0][-1]
-        license_plate_text = re.sub(r"[^A-Z0-9]", "", license_plate_text)
-        result = {
-            "plate": license_plate_text,
-            "score": conf_score,
-        }
-    else:
-        result = {
-            "plate": "",
-            "score": 0.0,
-        }
+    license_plate = "".join(license_plate_list)
+    result = {"plate": license_plate}
     return result
 
 
-def detect_on_img(img_path: os.path) -> Dict:
-    image = preprocess_image_for_ocr(img_path)
-    image = enhance_image_contrast(image)
-    image = enhance_image_brightness(image)
-    image = enhance_image_sharpening(image)
-    result = process_image_with_ocr(image)
-    return result
+image_path = "images/car6.jpeg"
+detect = detect_on_img(image_path)
+print(detect)
 
 
-image_path = "images/image9.jpg"
-
-text = detect_on_img(image_path)
-print(text)
+# cv2.imshow("License Plate Threshold", license_plate_thresh)
+# key = cv2.waitKey(0) & 0xFF
+# if key == 27 or key == ord("q"):
+#     cv2.destroyAllWindows()
