6c5daaaded8f011af4f76f0922e669fb53cd0cc1
Linas Stonkus <liniusst@gmail.com>
2023-12-01 16:39:05 +0200

    update code

--- app.py
+++ app.py
@@ -3,46 +3,87 @@ import os
 import cv2
 import easyocr
 import re
-from typing import Dict
+import numpy as np
+
+model_path = os.path.join(".", "runs", "detect", "train3", "weights", "best.pt")
+model = YOLO(model_path)
 
 
-def detect_on_img(img_path: os.path) -> Dict:
-    model_path = os.path.join(".", "runs", "detect", "train3", "weights", "best.pt")
-    model = YOLO(model_path)
+class detect_license_plate:
+    def __init__(self, img_path) -> None:
+        self.img_path = img_path
+        self.result = None
 
-    image = cv2.imread(img_path)
+    def _img_to_tresh(self):
+        try:
+            cap = cv2.VideoCapture(self.img_path)
+            ret, image = cap.read()
+            cap.release()
+            if not ret:
+                print("Error reading image.")
+                return
 
-    detections = model(image)[0]
-    reader = easyocr.Reader(["en"], gpu=False)
-    license_plate_list = []
+            detections = model(image)[0]
+            self.img_tresh = [
+                cv2.threshold(
+                    cv2.cvtColor(
+                        image[int(y1) : int(y2), int(x1) : int(x2)], cv2.COLOR_BGR2GRAY
+                    ),
+                    64,
+                    255,
+                    cv2.THRESH_BINARY_INV,
+                )[1]
+                for x1, y1, x2, y2, _, _ in detections.boxes.data.tolist()
+            ]
 
-    for detection in detections.boxes.data.tolist():
-        x1, y1, x2, y2, score, class_id = detection
-        cropped_plate = image[int(y1) : int(y2), int(x1) : int(x2)]
-        gray_plate = cv2.cvtColor(cropped_plate, cv2.COLOR_BGR2RGB)
-        _, license_plate_thresh = cv2.threshold(
-            gray_plate, 64, 255, cv2.THRESH_BINARY_INV
-        )
+        except Exception as e:
+            print(f"Error during image processing: {e}")
+            self.img_tresh = []
 
-    output = reader.readtext(license_plate_thresh, paragraph=False)
+    def img_cnts(self):
+        if not hasattr(self, "img_tresh") or not self.img_tresh:
+            return []
 
-    for out in output:
-        text_bbox, text, text_score = out
-        if text_score > 0.25:
-            text = re.sub(r"[^A-Z0-9]", "", text)
-            license_plate_list.append(text)
+        try:
+            reader = easyocr.Reader(["en"], gpu=False)
+            cnts, _ = cv2.findContours(
+                np.vstack(self.img_tresh).astype(np.uint8),
+                cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE,
+            )
+            cnt = max(cnts, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(cnt)
+            output = reader.readtext(np.vstack(self.img_tresh), paragraph=False)
+        except Exception as e:
+            print(f"Error during contour extraction or OCR: {e}")
+            output = []
 
-    license_plate = "".join(license_plate_list)
-    result = {"plate": license_plate}
-    return result
+        return output
+
+    def return_txt(self):
+        try:
+            self._img_to_tresh()
+            output = self.img_cnts()
+            license_plate_list = [
+                re.sub(r"[^A-Z0-9]", "", text)
+                for _, text, text_score in output
+                if text_score > 0.7
+            ]
+        except Exception as e:
+            print(f"Error during text processing: {e}")
+            license_plate_list = []
+
+        license_plate = "".join(license_plate_list)
+        self.result = {"plate": license_plate}
+        return self.result
 
 
-image_path = "images/car6.jpeg"
-detect = detect_on_img(image_path)
-print(detect)
-
-
-# cv2.imshow("License Plate Threshold", license_plate_thresh)
-# key = cv2.waitKey(0) & 0xFF
-# if key == 27 or key == ord("q"):
-#     cv2.destroyAllWindows()
+# Usage
+try:
+    image_path = "images/car6.jpeg"
+    detection = detect_license_plate(image_path)
+    result = detection.return_txt()
+    print(result)
+except Exception as e:
+    print(f"Error during license plate detection: {e}")
+    # Handle the error or exit the program
---
+++ utils.py
@@ -0,0 +1,3 @@
+class convert_img_to_text:
+    def __init__(self) -> None:
+        self.plates = []
