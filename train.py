b5baee9389d108077c8fa4ee665e9fd964ba67ed
Linas Stonkus <liniusst@gmail.com>
2023-12-19 20:10:17 +0200

    add train module

--- train.py
+++ train.py
@@ -2,4 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
 
-model.train(data="config.yaml", epochs=10)
+# model.train(data="config.yaml", epochs=10)
+model.train(data="data.yaml", epochs=1)
