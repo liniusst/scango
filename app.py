343259b12d25ffa254e9e8c32c27db182a897acc
Linas Stonkus <liniusst@gmail.com>
2023-12-19 20:09:55 +0200

    update with plt

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from ultralytics import YOLO
 import os
 import cv2
 import easyocr
-import re
+
 import numpy as np
 import torch
+import time
+import matplotlib.pyplot as plt
 
 torch.device("cpu")
 model_path = os.path.join(".", "runs", "detect", "train3", "weights", "best.pt")
@@ -12,90 +14,84 @@ model = YOLO(model_path)
 
 
 class detect_license_plate:
-    def __init__(self, img_path) -> None:
+    def __init__(self, img_path, lang=["en"]) -> None:
+        self.allow_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.reader = easyocr.Reader(lang, gpu=False, verbose=False)
         self.img_path = img_path
         self.result = None
 
-    def _read_image(self):
-        try:
-            cap = cv2.VideoCapture(self.img_path)
-            ret, image = cap.read()
-            cap.release()
-            if not ret:
-                print("Error reading image.")
-                return None
-            return image
-        except Exception as e:
-            print(f"Error during image reading: {e}")
-            return None
-
-    def _img_to_tresh(self):
-        image = self._read_image()
-        if image is None:
-            self.img_tresh = []
-            return
-
-        try:
+    def _read_image_to_tresh(self):
+        cap = cv2.VideoCapture(self.img_path)
+        _, image = cap.read()
+        cap.release()
+        with torch.no_grad():
             detections = model(image)[0]
-            self.img_tresh = [
-                cv2.threshold(
-                    cv2.cvtColor(
-                        image[int(y1) : int(y2), int(x1) : int(x2)], cv2.COLOR_BGR2GRAY
-                    ),
-                    64,
-                    255,
-                    cv2.THRESH_BINARY_INV,
-                )[1]
-                for x1, y1, x2, y2, _, _ in detections.boxes.data.tolist()
-            ]
-        except Exception as e:
-            print(f"Error during image processing: {e}")
-            self.img_tresh = []
+
+        self.img_tresh = [
+            self._process_detection(image, box)
+            for box in detections.boxes.data.tolist()
+        ]
+
+    def _process_detection(self, image, box):
+        x1, y1, x2, y2, _, _ = box
+
+        region_of_interest = image[int(y1) : int(y2), int(x1) : int(x2)]
+        gray_image = cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2GRAY)
+        bfilter = cv2.bilateralFilter(gray_image, 11, 17, 17)
+        _, thresholded = cv2.threshold(gray_image, 64, 255, cv2.THRESH_BINARY_INV)
+
+        plt.subplot(2, 2, 1)
+        plt.imshow(cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2RGB))
+        plt.title("Original Region")
+
+        plt.subplot(2, 2, 2)
+        plt.imshow(gray_image)
+        plt.title("Gray Image")
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(bfilter)
+        plt.title("Gray after filter Image")
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(thresholded)
+        plt.title("Thresholded Image")
+
+        plt.show()
+
+        return thresholded
+
+    # ideti filtra
 
     def img_cnts(self):
-        if not hasattr(self, "img_tresh") or not self.img_tresh:
-            return []
+        cnts, _ = cv2.findContours(
+            np.vstack(self.img_tresh).astype(np.uint8),
+            cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE,
+        )
+        cnt = max(cnts, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(cnt)
 
-        try:
-            reader = easyocr.Reader(["en"], gpu=False, verbose=False)
-            cnts, _ = cv2.findContours(
-                np.vstack(self.img_tresh).astype(np.uint8),
-                cv2.RETR_EXTERNAL,
-                cv2.CHAIN_APPROX_SIMPLE,
-            )
-            cnt = max(cnts, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(cnt)
-            output = reader.readtext(np.vstack(self.img_tresh), paragraph=False)
-        except Exception as e:
-            print(f"Error during contour extraction or OCR: {e}")
-            output = []
-
+        output = self.reader.readtext(
+            np.vstack(self.img_tresh), allowlist=self.allow_list, paragraph=False
+        )
         return output
 
     def return_txt(self):
-        try:
-            self._img_to_tresh()
-            output = self.img_cnts()
-            license_plate_list = [
-                re.sub(r"[^A-Z0-9]", "", text)
-                for _, text, text_score in output
-                if text_score > 0.25
-            ]
-        except Exception as e:
-            print(f"Error during text processing: {e}")
-            license_plate_list = []
-
-        license_plate = "".join(license_plate_list)
-        self.result = {"plate": license_plate}
-        return self.result
+        t0 = time.time()
+        self._read_image_to_tresh()
+        output = self.img_cnts()
+        text = [x[1] for x in output]
+        confid = [x[2] for x in output]
+        text = "".join(text) if len(text) > 0 else None
+        confid = np.round(np.mean(confid), 2) if len(confid) > 0 else None
+        t1 = time.time()
+        print(
+            f"Recognized number: {text}, conf.:{confid}.\nOCR total time: {(t1 - t0):.3f}s"
+        )
 
 
-# Usage
-try:
-    image_path = "images/car5.jpeg"
-    detection = detect_license_plate(image_path)
-    result = detection.return_txt()
-    print(result)
-except Exception as e:
-    print(f"Error during license plate detection: {e}")
-    # Handle the error or exit the program
+image_path = "images/autogidas2.jpg"
+detection = detect_license_plate(image_path)
+result = detection.return_txt()
+
+# print(dir(cv2))
